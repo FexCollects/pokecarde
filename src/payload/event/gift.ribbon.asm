0d6b8752157bccfe0b233a5468119d7e4d3859ee
Fex Collects <fex@fexcollects.com>
2026-01-05 05:23:05 +0000

    add ribbon

---
+++ src/payload/event/gift.ribbon.asm
@@ -0,0 +1,18 @@
+SECTION "payload/event/gift.ribbon", ROM0
+INCLUDE "constants/ribbons.asm"
+INCLUDE "include/event.asm"
+INCLUDE "include/charmaps.asm"
+
+DataPacket::
+	Mystery_Event
+	GBAPtr DataStart
+	GBAPtr DataEnd
+
+DataStart:
+	db AWARD_RIBBON 
+	db MARINE_RIBBON
+	db REGIONAL_TOURNEY_CHAMPION_2003
+	db END_OF_CHUNKS
+
+DataEnd:
+	db 0,0,0 ; padding
