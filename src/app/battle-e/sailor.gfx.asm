0efa38dcc19000c72372d4812d90f43add16ad5b
Fex Collects <fex@fexcollects.com>
2026-01-24 20:26:33 +0000

    Use new struct on trainers gfx

--- src/app/battle-e/sailor.gfx.asm
+++ src/app/battle-e/sailor.gfx.asm
@@ -1,9 +1,17 @@
 SECTION "app/battle-e/sailor.gfx", ROM0
+INCLUDE "include/erapi.asm"
 
 TrainerSprite:: ; 0F38
     INCBIN "build/gfx/trainers/sailor.4bpp"
 
-TrainerSpriteData:: ; 1738
-    dw TrainerSprite
-    dw TrainerPalette
-    db $08,$08,$01,$01,$01,$01,$01
+dstruct ER_CustomSprite, \
+  TrainerSpriteData, \
+    .TilePtr=TrainerSprite, \
+    .PalettePtr=TrainerPalette, \
+    .Width=8, \
+    .Height=8, \
+    .FramesPerBank=1, \
+    .Unknown=1, \
+    .HitBoxWidth=1, \
+    .HitBoxHeight=1, \
+    .FrameCount=1
